0f54f71ba0d078ebaaf116d3efb99dc01965d403
Baris Kanber <b.kanber@ucl.ac.uk>
2024-02-08 13:37:51 +0100

    Remove docker save

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ version="$(cat version.txt)"
 
 sudo docker build -t xnat_pseudo_anonymizer:${version} .
 
-sudo docker save -o xnat_pseudo_anonymizer-${version}.tar xnat_pseudo_anonymizer:${version}
+# sudo docker save -o xnat_pseudo_anonymizer-${version}.tar xnat_pseudo_anonymizer:${version}
 
